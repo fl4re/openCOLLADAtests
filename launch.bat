06a00b884cfe030c0a76b775a8f4e461b30b14ed
Jerome Jacob <jerome.jacob@starbreeze.com>
2017-02-20 11:07:55 +0100

    Don't add these files.

---
+++ launch.bat
@@ -0,0 +1,12 @@
+setlocal
+
+set PATH_BACKUP=%PATH%
+
+set OPENCOLLADA_PATH=E:\fl4re\OpenCOLLADA
+set MAYA_PATH2015_X64=E:\Program Files\Autodesk\Maya2015
+
+set PATH=C:\Python27_;%PATH%
+
+python launch.py
+
+set PATH=%PATH_BACKUP%
